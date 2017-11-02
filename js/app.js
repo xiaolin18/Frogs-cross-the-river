5785aa8958209727d822f54a6b26ea36d05703f1
xiaolin <xiaolin9915@gmail.com>
2017-11-02 23:42:48 +0800

    fix(实例化): 实例化出多个敌人

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // 这是我们的玩家要躲避的敌人
-var Enemy = function(x, y) {
-    // 要应用到每个敌人的实例的变量写在这里
+var Enemy = function(x, y, speed) {
+    // 要应用到每，个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
-    this.speed = 50;
+    this.speed = speed;
     this.x = x;
     this.y = y;
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
@@ -21,7 +21,7 @@ Enemy.prototype.update = function(dt) {
 };
 // 碰撞检测
 Enemy.prototype.checkCollision = function(enemy, player) {
-  if (enemy.y === player.y) {
+  if (enemy.y === player.y && (enemy.x - player.x <= 50)) {
     player.x = 200;
     player.y = 400;
   }
@@ -33,10 +33,10 @@ Enemy.prototype.render = function() {
 
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
-var Player = function(x, y) {
+var Player = function(x, y, speed) {
   this.x = x;
   this.y = y;
-  this.speed = 10;
+  this.speed = speed;
   this.sprite = 'images/char-boy.png';
 };
 Player.prototype.update = function(dt) {
@@ -76,11 +76,14 @@ Player.prototype.render = function() {
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
-let enemy = new Enemy(0, 250);
-let enemy1 = new Enemy(10, 300);
+// 如何实例化多个敌人（位置不同， 速度不同）
+let enemy = new Enemy(0, 50, 50);
+let enemy1 = new Enemy(10, 100, 20);
+let enemy2 = new Enemy(10, 100, 30);
 
-let allEnemies = [enemy, enemy1];
-let player = new Player(200, 400);
+
+let allEnemies = [enemy, enemy1, enemy2];
+let player = new Player(200, 400, 5);
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
